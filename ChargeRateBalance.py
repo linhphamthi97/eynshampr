3884a3ef5417d2b8cc3715362d77c0e76da26787
linhphamthi97 <linh.phamthi@balliol.ox.ac.uk>
2019-02-18 11:28:39 +0000

    Plot SOC graph

--- ChargeRateBalance.py
+++ ChargeRateBalance.py
@@ -4,42 +4,63 @@ Created on Sun Feb 10 22:41:41 2019
 
 @author: Linh Pham Thi
 
-This file contains the function to balance the charging rates among the charging cars
+This file contains the function to balance the charging rates among the charging cars.
 Inputs: EV battery, settings and datagen variables
 Output: Sum of all the chargerates
 """
 import settings
 import numpy as np
+import matplotlib.pyplot as plt
 
 def ChargeRateBalance(evbatt):
     # Initialising variables
     total_weigh = 0
     total_chargerate = 0
+    pv_energy_available = settings.pv_energy_profile[settings.hour]
+    SOC_plot=list()
+#    extra_charging=list()
     
-    # Terms for the weighted average
-    for n in range(1,settings.carnumber+1):
-        evbatt["EV{0}".format(n)].rel_weigh = evbatt["EV{0}".format(n)].fill / evbatt["EV{0}".format(n)].time
-        total_weigh = total_weigh + evbatt["EV{0}".format(n)].rel_weigh
     
     # Calculating chargerate for each car and charge
     for n in range(1,settings.carnumber+1):
         print('EV',n, 'SOC before charging: ',evbatt["EV{0}".format(n)].SOC)
         
-        chargerate = settings.pv_energy_profile[settings.hour] * evbatt["EV{0}".format(n)].rel_weigh / total_weigh
+        # Terms for the weighted average
+        for k in range(n,settings.carnumber+1):
+            evbatt["EV{0}".format(k)].rel_weigh = evbatt["EV{0}".format(k)].fill / evbatt["EV{0}".format(k)].time
+            total_weigh = total_weigh + evbatt["EV{0}".format(k)].rel_weigh       
         
+        evbatt["EV{0}".format(n)].chargerate = pv_energy_available * evbatt["EV{0}".format(n)].rel_weigh / total_weigh
+        
+        # Implement restrictions on charging rates imposed by charging type
         if evbatt["EV{0}".format(n)].chargetype == 0:       #Slow charge
-            chargerate = np.clip(chargerate,0,settings.slowcharge_ulim)
+            evbatt["EV{0}".format(n)].chargerate = np.clip(evbatt["EV{0}".format(n)].chargerate,0,settings.slowcharge_ulim)
         else: 
-            chargerate = np.clip(chargerate,0,settings.fastcharge_ulim)
+            evbatt["EV{0}".format(n)].chargerate = np.clip(evbatt["EV{0}".format(n)].chargerate,0,settings.fastcharge_ulim)
         
+        # Total energy left
+        pv_energy_available = pv_energy_available - evbatt["EV{0}".format(n)].chargerate
         
-        # total_chargerate = total_chargerate + chargerate
-                
+                 
         # Charging
-        evbatt["EV{0}".format(n)].charge(chargerate,settings.t_inc)
+        evbatt["EV{0}".format(n)].charge(evbatt["EV{0}".format(n)].chargerate,settings.t_inc)
         
-        print('EV',n, 'Chargerate: ', chargerate)
+        print('EV',n, 'Chargerate: ', evbatt["EV{0}".format(n)].chargerate)
         print('EV',n, 'SOC after charging: ',evbatt["EV{0}".format(n)].SOC)
         print('')
         
+        # For plotting
+        SOC_plot.append(evbatt["EV{0}".format(n)].SOC * 100)
+        
+ 
+    print('Leftover energy: ', pv_energy_available, ' kWh')
+    
+    # Plotting a graph of the SOC
+    y_axis = np.arange(len(SOC_plot)) 
+    plt.barh(y_axis, SOC_plot)
+    plt.title('State of charge of the EVs')
+    plt.xlabel('State of charge [%]')
+    plt.show()
+      
+    
     return evbatt, total_chargerate
--- main.py
+++ main.py
@@ -8,11 +8,7 @@ author: Linh Pham Thi
 # =============================================================================
 # Importing neccessary Python libraries and modules
 # =============================================================================
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import random
-from EVbattery import EVbattery
 from ChargeRateBalance import ChargeRateBalance
 import settings
 import datagen
@@ -25,14 +21,9 @@ evbatt, total_ev_demand, total_inst_chargerate = datagen.datagen()
 # pv_energy_profile = np.loadtxt('total_' + str(settings.month) + '_kWh.txt')
 gridenergy = total_inst_chargerate - settings.pv_energy_profile[settings.hour]
 
-#print('MOMENTARY DATA')
-#print('Momentary demand: ', total_chargerate, 'kW')
-#print('Momentary PV output: ', pv_energy_profile[hour], 'kW')
-#print('Momentary energy we have to buy: ', gridenergy, 'kW')
-#print('')
-
 # Total energy balance based on expected daily PV output profile
-total_pv_energy = np.trapz(settings.pv_energy_profile[settings.hour:],x=settings.t,dx=settings.t_inc) #Using trapezoidal rule to integrate area under curve
+# Using trapezoidal rule to integrate area under curve
+total_pv_energy = np.trapz(settings.pv_energy_profile[settings.hour:],x=settings.t,dx=settings.t_inc) 
 total_gridenergy = total_ev_demand - total_pv_energy
 
 print('DAILY DATA')
@@ -40,6 +31,6 @@ print('Total demand: ', total_ev_demand, 'kWh')
 print('Total PV output: ', total_pv_energy, 'kWh')
 print('Total energy we have to buy: ', total_gridenergy, 'kWh')
 print('Total we have to pay for the electricity bought: ', total_gridenergy * settings.el_price, 'GBP')
+print('')
         
-
 evbatt, total_chargerate = ChargeRateBalance(evbatt)
