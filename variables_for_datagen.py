4fd9afa320141207f1acf0dbf1e1ed375fff3dfe
nicolekuek <46975436+nicolekuek@users.noreply.github.com>
2019-02-28 13:42:06 +0000

    Datagen variable distributions

---
+++ variables_for_datagen.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 22 16:54:47 2019
+
+@author: Nicole
+"""
+
+#%% LENGTH OF STAY
+#2nd method
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy import stats
+from scipy.stats import gamma
+
+mu = 10
+sigma = 2.1
+x = np.linspace(mu - 4*sigma, mu + 4*sigma, 100)
+plt.plot(x, stats.norm.pdf(x, mu, sigma))
+plt.title('Distribution representing length of stay in P&R')
+plt.ylabel('Probability')
+plt.xlabel('Time parked (hours)')
+
+plt.show()
+
+#%% EV BATTERY CAPACITY
+
+# From report, typical capacities (all Li-ion batteries) are:
+
+#corresponding to Renault Twizzy, Hyundai Ioniq, Nissan Leaf, VW E-Golf, Tesla Model S
+capacity = [6.1,6.1,28,28,28,28,28,28,28,28,28,28,30,30,30,30,30,30,30,30,30,30,24.2,24.2,24.2,24.2,24.2,24.2,24.2,24.2,24.2,24.2,100,100,100]
+bins = 100
+plt.figure()
+plt.hist(capacity, bins, normed=True, histtype='bar')
+
+x = np.linspace(0, 120, 100) #range of kWh 
+
+## lets try the normal distribution first
+#mu, sigma = stats.norm.fit(capacity) # get mean and standard deviation
+#pdf_g = 10*stats.norm.pdf(x,mu,sigma) # now get theoretical values in our interval  
+#plt.plot(x, pdf_g, label="Normal") # plot it
+
+# gamma dist - best
+ag,bg,cg = gamma.fit(capacity)  
+pdf_gamma = 10*gamma.pdf(x,ag,bg,cg)  
+plt.plot(x, pdf_gamma, label="Gamma")
+
+## beta dist
+#ab,bb,cb,db = stats.beta.fit(capacity)  
+#pdf_beta = 10*stats.beta.pdf(x,ab,bb,cb,db)  
+#plt.plot(x, pdf_beta, label="Beta")
+
+plt.title('Distribution of popular EV battery capacities')
+plt.xlabel('kWh')
+plt.ylabel('Probability')
+plt.legend()
+plt.show()
+
+#%% SOC on arrival
+
+# use EV battery capacity distribution
+
+mu = 18
+sigma = 2
+x = np.linspace(2, 25, 100)
+pdf_g = stats.norm.pdf(x,mu,sigma)
+plt.plot(x, pdf_g)
+plt.title('Distribution representing distance travelled to P&R')
+plt.ylabel('Probability')
+plt.xlabel('Distance (miles)')
+
+plt.show()
+
+plt.figure()
+consumption = 0.293 #kWh/mile
+SOC = [(i - mu*consumption)/i for i in capacity] #assuming all travelling 4 miles
+#print(SOC)
+
+bins = 50
+plt.hist(SOC, bins, normed=1, histtype='bar')
+x = np.linspace(0,1,100)
+
+ab,bb,cb,db = stats.beta.fit(SOC)  
+pdf_beta = stats.beta.pdf(x,ab,bb,cb,db)  
+plt.plot(x,pdf_beta)
+
+plt.title('Distribution of SOC on arrival')
+plt.xlabel('Proportion of charge')
+plt.ylabel
+
+#%% Arrival time
+
+x = range(0,25)
+y = pd.read_csv('Arrivaltime.csv', usecols=[1])
+arrival = y.values
+plt.figure()
+plt.plot(x,y)
+plt.title('Distribution of influx of cars')
+plt.xlabel('Time of day (hour)')
+plt.ylabel('Number of cars')
