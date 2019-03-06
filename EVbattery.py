622f2a9c24fbf4a3c4a6b270d292c2c670955a6b
nicolekuek <46975436+nicolekuek@users.noreply.github.com>
2019-03-06 16:41:46 +0000

    trial inputs

--- EVbattery.py
+++ EVbattery.py
@@ -7,14 +7,15 @@ Created on Sun Feb 10 14:46:35 2019
 
 class EVbattery:
     
-    def __init__(self,capacity,SOC,time,ctype):
+    def __init__(self,capacity,SOC,time,ctype,arrivaltime):
         self.capacity = capacity # kWh
         self.SOC = SOC # proportion charged when arriving in P+R
         self.time = time # length of time car will be parked in hours (driver inputs on arrival)
         self.fill = self.capacity - self.capacity * self.SOC # kWh needed to completely fill battery
         self.avg_chargerate = self.fill / self.time # actual kWh given length of stay
         self.chargetype = ctype    # Type of charging (slow=0, fast=1) 
-
+        self.arrivaltime = arrivaltime
+        
     def charge(self,chargerate,t_inc):
         
         self.fill = self.fill - chargerate*t_inc
--- variables_for_datagen.py
+++ variables_for_datagen.py
@@ -13,17 +13,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from scipy import stats
-from scipy.stats import gamma
 
-mu = 10
-sigma = 2.1
-x = np.linspace(mu - 4*sigma, mu + 4*sigma, 100)
-plt.plot(x, stats.norm.pdf(x, mu, sigma))
-plt.title('Distribution representing length of stay in P&R')
-plt.ylabel('Probability')
-plt.xlabel('Time parked (hours)')
+#mu = 10
+#sigma = 2.1
+#x = np.linspace(mu - 4*sigma, mu + 4*sigma, 100)
+#plt.plot(x, stats.norm.pdf(x, mu, sigma))
+#plt.title('Distribution representing length of stay in P&R')
+#plt.ylabel('Probability')
+#plt.xlabel('Time parked (hours)')
+#
+#plt.show()
 
-plt.show()
+time = np.random.normal(10,2.1)
+print(time)
 
 #%% EV BATTERY CAPACITY
 
@@ -39,17 +41,17 @@ x = np.linspace(0, 120, 100) #range of kWh
 
 ## lets try the normal distribution first
 #mu, sigma = stats.norm.fit(capacity) # get mean and standard deviation
-#pdf_g = 10*stats.norm.pdf(x,mu,sigma) # now get theoretical values in our interval  
+#pdf_g = stats.norm.pdf(x,mu,sigma) # now get theoretical values in our interval  
 #plt.plot(x, pdf_g, label="Normal") # plot it
 
 # gamma dist - best
-ag,bg,cg = gamma.fit(capacity)  
-pdf_gamma = 10*gamma.pdf(x,ag,bg,cg)  
-plt.plot(x, pdf_gamma, label="Gamma")
+ag,bg,cg = stats.gamma.fit(capacity)  
+pdf_gamma = stats.gamma.pdf(x,ag,bg,cg)  
+plt.plot(x, pdf_gamma)
 
 ## beta dist
 #ab,bb,cb,db = stats.beta.fit(capacity)  
-#pdf_beta = 10*stats.beta.pdf(x,ab,bb,cb,db)  
+#pdf_beta = stats.beta.pdf(x,ab,bb,cb,db)  
 #plt.plot(x, pdf_beta, label="Beta")
 
 plt.title('Distribution of popular EV battery capacities')
@@ -58,13 +60,19 @@ plt.ylabel('Probability')
 plt.legend()
 plt.show()
 
+# clearly gamma distribution fits best
+# mu = 32kWh from Sam's data
+# shape = sqrt(mu), scale = shape
+shape, scale = 5.66,7
+capacity = np.random.gamma(shape, scale)
+
 #%% SOC on arrival
 
 # use EV battery capacity distribution
 
 mu = 18
-sigma = 2
-x = np.linspace(2, 25, 100)
+sigma = 3
+x = np.linspace(2, 30, 100)
 pdf_g = stats.norm.pdf(x,mu,sigma)
 plt.plot(x, pdf_g)
 plt.title('Distribution representing distance travelled to P&R')
@@ -82,13 +90,21 @@ bins = 50
 plt.hist(SOC, bins, normed=1, histtype='bar')
 x = np.linspace(0,1,100)
 
+#ag,bg,cg  = stats.gamma.fit(SOC)  
+#pdf_gamma = stats.gamma.pdf(x,ag,bg,cg)  
+#plt.plot(x,pdf_gamma,label='Gamma')
+
 ab,bb,cb,db = stats.beta.fit(SOC)  
 pdf_beta = stats.beta.pdf(x,ab,bb,cb,db)  
-plt.plot(x,pdf_beta)
+plt.plot(x,pdf_beta,label='Beta')
+
+a, b = 2, 1.7
+SOC = np.random.beta(a,b)
 
 plt.title('Distribution of SOC on arrival')
 plt.xlabel('Proportion of charge')
-plt.ylabel
+plt.ylabel('Frequency')
+plt.legend()
 
 #%% Arrival time
 
